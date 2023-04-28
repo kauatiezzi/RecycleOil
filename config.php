a673d5094ad73e4ddfd6961d505821060cf75e9d
kauatiezzi <kauatiezzi02@hotmail.com>
2023-04-28 15:45:41 -0300

    config.php fix

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
-<?php
+<!-- 
 
-       $dbHost = 'aws.connect.psdb.cloud';
-       $dbUsername = 'xgsr2zh92tn882fcbpp7';
-       $dbPassword = 'xgsr2zh92tn882fcbpp7';
-       $dbName = 'formulario_uniaraoil';
-       $dbssl = '/etc/ssl/cert.pem';
+      //  $dbHost = 'aws.connect.psdb.cloud';
+      //  $dbUsername = 'xgsr2zh92tn882fcbpp7';
+      //  $dbPassword = 'xgsr2zh92tn882fcbpp7';
+      //  $dbName = 'formulario_uniaraoil';
+      //  $dbssl = '/etc/ssl/cert.pem';
 
-       $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName,$dbssl);
+      //  $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName,$dbssl);
 
       //  if($conexao->connect_errno)
       //  {
@@ -16,4 +16,12 @@
       //  {
       //   echo "Conectado";
       //  }
+ -->
+
+<?php
+  $conexao = "mysql:host={aws.connect.psdb.cloud};dbname={formulario_uniaraoil}";
+  $options = array(
+    PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt",
+  );
+  $pdo = new PDO($conexao, ["xgsr2zh92tn882fcbpp7"], ["pscale_pw_9sjA5oIGkgJPUVuxpaMVgaN57HrSWpY1jphLWzMhnLC"], $options);
 ?>
