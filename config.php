6489618e92be745ab0f541fc9c8798121dc985ae
kauatiezzi <kauatiezzi02@hotmail.com>
2023-04-28 14:46:35 -0300

    fix config.php

--- config.php
+++ config.php
@@ -1,16 +1,18 @@
 <?php
 
-      //  $dbHost = 'Localhost';
-      //  $dbUsername = 'root';
-      //  $dbPassword = '';
-      //  $dbName = 'formulario_uniaraoil';
+       $dbHost = 'aws.connect.psdb.cloud';
+       $dbUsername = 'xgsr2zh92tn882fcbpp7';
+       $dbPassword = 'pscale_pw_9sjA5oIGkgJPUVuxpaMVgaN57HrSWpY1jphLWzMhnLC';
+       $dbName = 'formulario_uniaraoil';
 
-      //  $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
-      $conexao = mysqli_init();
-      $conexao->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-      $conexao->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
-      $conexao->close();
-      
+       $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+//   $mysqli = mysqli_init();
+//   $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+//   $mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
+//   $mysqli->close();
+
+
       //  if($conexao->connect_errno)
       //  {
       //   echo "Erro";
