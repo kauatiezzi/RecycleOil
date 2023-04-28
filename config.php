297a9df095268176e3e8967590211028e5ad1b22
kauatiezzi <kauatiezzi02@hotmail.com>
2023-04-28 14:53:15 -0300

    config.php

--- config.php
+++ config.php
@@ -1,17 +1,16 @@
 <?php
 
-       $dbHost = 'aws.connect.psdb.cloud';
-       $dbUsername = 'xgsr2zh92tn882fcbpp7';
-       $dbPassword = 'pscale_pw_9sjA5oIGkgJPUVuxpaMVgaN57HrSWpY1jphLWzMhnLC';
-       $dbName = 'formulario_uniaraoil';
-       $dbSSL = ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+      //  $dbHost = 'Localhost';
+      //  $dbUsername = 'root';
+      //  $dbPassword = '';
+      //  $dbName = 'formulario_uniaraoil';
 
-       $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName,$dbSSL);
+      //  $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
 
-//   $mysqli = mysqli_init();
-//   $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-//   $mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
-//   $mysqli->close();
+  $mysqli = mysqli_init();
+  $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+  $mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
+  $mysqli->close();
 
 
       //  if($conexao->connect_errno)
