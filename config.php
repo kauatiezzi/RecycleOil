006a9fabb5f14ce91b2facfdf3f5b6f37a9e9c67
kauatiezzi <kauatiezzi02@hotmail.com>
2023-04-28 23:43:35 -0300

    fix config.php

--- config.php
+++ config.php
@@ -21,37 +21,12 @@ $dbUsername = $_ENV['USERNAME'];
 $dbPassword = $_ENV['PASSWORD'];
 $dbName = $_ENV['DATABASE'];
 
-// Configuração do SSL
-$sslOptions = array(
-    MYSQLI_CLIENT_SSL_CERT => NULL,
-    MYSQLI_CLIENT_SSL_KEY => NULL,
-    MYSQLI_CLIENT_SSL_CA => "/etc/ssl/certs/ca-certificates.crt",
-    MYSQLI_CLIENT_SSL_CIPHER => NULL,
-    MYSQLI_CLIENT_SSL_VERIFY_SERVER_CERT => true,
-);
-
-// Conexão ao banco de dados com SSL
-$conexao = mysqli_init();
-$conexao->ssl_set(
-    $sslOptions['MYSQLI_CLIENT_SSL_CERT'],
-    $sslOptions['MYSQLI_CLIENT_SSL_KEY'],
-    $sslOptions['MYSQLI_CLIENT_SSL_CA'],
-    $sslOptions['MYSQLI_CLIENT_SSL_CIPHER'],
-    $sslOptions['MYSQLI_CLIENT_SSL_VERIFY_SERVER_CERT']
-);
-$conexao->real_connect($dbHost, $dbUsername, $dbPassword, $dbName);
-
-// Verificação de erros
-if (mysqli_connect_errno()) {
-    echo "Erro ao conectar ao banco de dados MySQL: " . mysqli_connect_error();
-    exit();
+$mysqli = mysqli_init();
+$mysqli->ssl_set(NULL, NULL, '/etc/ssl/certs/ca-certificates.crt', NULL, NULL);
+$conexao = $mysqli->real_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+if (!$conexao) {
+  die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
 
-// Execução de consultas e outras operações no banco de dados
-// ...
-
-// Fechamento da conexão com o banco de dados
-mysqli_close($conexao);
-?>
 
 ?>
